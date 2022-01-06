6bd95f45e2ae43bbbc40501b83d84b12ab508e0e
gosutek <gosutek@gmail.com>
2022-01-07 00:58:04 +0200

    removed express from backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const Sequelize = require('sequelize')
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/config/config.json")[env];
 
 const db = require('./models')
 
-const PORT = 9103;
-
-const app = express();
-
-async function checkDatabaseConnection() {
-    try {
-        await db.sequelize.authenticate();
-        console.log('Connection to database has been established successfully');;
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-        process.exit(1);
-    }
-}
-
-async function init() {
-    await checkDatabaseConnection();
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    })
-}
-init();
+module.exports = {
+	checkDatabaseConnection: async function () {
+		try {
+			await db.sequelize.authenticate();
+			console.log("Succefully connected to database!");
+			return {
+				status: "OK",
+				dbconnection:
+					"username: " +
+					config.username +
+					" | password: " +
+					config.password +
+					" | database_name: " +
+					config.database +
+					" | dialect: " +
+					config.dialect
+			};
+		} catch (err) {
+			console.log("Connection to database failed ->" + err.stack);
+			return { status: "Failed" };
+		}
+	},
+	// add more functions here
+};
